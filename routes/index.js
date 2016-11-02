752580374d6f0297598060a03b44de4580db987b
dongyanghong <dongyanghongfe@gmail.com>
2016-11-02 21:04:08 +0800

    删除路由无用

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose'); //mongo connection
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
- 
 router.post('/login', function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
